d5559fa972f42fe9d3c3e1fa338c03de98dc1b90
Eshu-M <eshambehredin20@gmail.com>
2023-08-30 21:19:45 -0700

    added search functionality

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import Providers from './Providers'
 import Header from './components/Header'
 import Navbar from './components/Navbar'
+import SearchBox from './components/SearchBox'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -26,6 +27,8 @@ export default function RootLayout({ children }) {
         <Navbar/>
 
         {/* SearchBox */}
+
+        <SearchBox/>
 
         {children}
         
